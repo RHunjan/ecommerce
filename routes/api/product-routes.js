245f356b10baa51ad309791952f11f2732091a20
ReenaH <Reena_Hunjan@yahoo.com>
2022-07-30 14:43:04 -0400

    messed something up

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        as: "tagged_products"
+      }
+      ]
+  }).then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
 
 // create new product
 router.post('/', (req, res) => {
